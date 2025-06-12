48238c7941d9ae8beece7c2365198b8f35a16a8e
LiuJialong <liujl01@sjtu.edu.cn>
2025-06-12 21:05:39 +0800

    [improve]: test framework

--- test/test_msgq.cpp
+++ test/test_msgq.cpp
@@ -11,28 +11,26 @@ using namespace ipc;
 void test_basic()
 {
     const char* msg = "Hello, IPC!";
+    size_t msg_size = strlen(msg) + 1; // +1 for null terminator
 
     pid_t pid = fork();
     if (pid == -1) {
-        ipc::node node("basic");
-        node.send(msg, strlen(msg) + 1);
+        perror("fork fail");
+        exit(1);
     } else if (pid == 0) {
         ipc::node node("basic");
-        std::vector<char> buffer(strlen(msg));
-        node.receive(buffer.data(), strlen(msg));
+        std::vector<char> buffer(msg_size);
+        node.receive(buffer.data(), msg_size);
 
-        EXPECT_EQ(strcmp(buffer.data(), msg), 0) << "Received message does not match sent message";
+        EXPECT_EQ(strcmp(buffer.data(), msg), 0) << "Received message: " << buffer.data();
+    } else {
+        ipc::node node("basic");
+        node.send(msg, msg_size);
+        wait(nullptr);
     }
 }
 
-TEST(IPC, Basic)
+TEST(MSGQ, basic)
 {
     test_basic();
-}
-
-int main(int argc, char** argv)
-{
-    printf("Running Google Test from %s\n", __FILE__);
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
 }
