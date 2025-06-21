9605acd3b23a574e3d04504ad21b68ef9e353a85
Jialong Liu <liujl01@sjtu.edu.cn>
2025-06-21 18:48:20 +0800

    [fix]: stderror warn in windows

--- include/ipc/assert.h
+++ include/ipc/assert.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#ifndef _WIN32
 #define ASSERT_EXIT(expr, format, ...)                 \
     do {                                               \
         if (static_cast<bool>(expr)) {                 \
@@ -17,3 +18,30 @@
             return ret;                                \
         }                                              \
     } while (0)
+#else
+#include <string>
+inline std::string safe_strerror(int errnum)
+{
+    char buffer[256];
+    strerror_s(buffer, sizeof(buffer), errnum);
+    return std::string(buffer);
+}
+
+#define ASSERT_EXIT(expr, format, ...)                        \
+    do {                                                      \
+        if (static_cast<bool>(expr)) {                        \
+            fprintf(stderr, "Error: " format ": %s\n",        \
+                ##__VA_ARGS__, safe_strerror(errno).c_str()); \
+            exit(EXIT_FAILURE);                               \
+        }                                                     \
+    } while (0)
+
+#define ASSERT_RETURN(expr, ret, format, ...)                 \
+    do {                                                      \
+        if (static_cast<bool>(expr)) {                        \
+            fprintf(stderr, "Error: " format ": %s\n",        \
+                ##__VA_ARGS__, safe_strerror(errno).c_str()); \
+            return ret;                                       \
+        }                                                     \
+    } while (0)
+#endif
