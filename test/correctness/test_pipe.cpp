bae65bd09e5b46fa012012d1ecf1a5ca8f52f384
Jialong Liu <liujl01@sjtu.edu.cn>
2025-06-23 10:50:03 +0800

    [fix]: pipe test

--- test/correctness/test_pipe.cpp
+++ test/correctness/test_pipe.cpp
@@ -12,39 +12,53 @@ void test_basic()
 {
     const char* msg = "Hello, IPC!";
 
-    ipc::node node("basic", ipc::ChannelType::NamedPipe);
-    EXPECT_TRUE(node.send(msg, strlen(msg)));
+    std::thread server_thread([msg]() {
+        ipc::node server_node("basic", ipc::LinkType::Receiver, ipc::ChannelType::NamedPipe);
+        auto rec = server_node.receive();
+        if (!rec) {
+            fprintf(stderr, "Server failed to receive message\n");
+            exit(1);
+        }
+        const char* res = static_cast<const char*>(rec.get());
+        EXPECT_STREQ(res, msg);
+    });
 
-    auto rec = node.receive();
-    if (!rec) {
-        fprintf(stderr, "Failed to receive message\n");
-        exit(1);
-    }
-    const char* res = static_cast<const char*>(rec.get());
-    EXPECT_STREQ(res, msg);
+    // Ensure that the server is started and waiting for connection
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    ipc::node client_node("basic", ipc::LinkType::Sender, ipc::ChannelType::NamedPipe);
+    EXPECT_TRUE(client_node.send(msg, strlen(msg)));
+
+    server_thread.join();
 }
 
 void test_loop()
 {
-    const char* base_msg = "Hello, IPC";
+    const char* msg = "Hello, IPC";
 
-    ipc::node node("loop", ipc::ChannelType::NamedPipe);
-    for (int i = 0; i < 20; ++i) {
-        std::string full_msg = std::string(base_msg) + " - Message #" + std::to_string(i + 1);
-        EXPECT_TRUE(node.send(full_msg.c_str(), full_msg.size() + 1)); // +1 for null terminator
-    }
-
-    std::shared_ptr<void> rec = nullptr;
-    for (int i = 0; i < 20; ++i) {
-        rec = node.receive();
-        if (!rec) {
-            fprintf(stderr, "Failed to receive message\n");
-            exit(1);
+    std::thread server_thread([msg]() {
+        ipc::node server_node("loop", ipc::LinkType::Receiver, ipc::ChannelType::NamedPipe);
+        for (int i = 0; i < 20; ++i) {
+            auto rec = server_node.receive();
+            if (!rec) {
+                fprintf(stderr, "Failed to receive message\n");
+                exit(1);
+            }
+            const char* res = static_cast<const char*>(rec.get());
+            std::string expected_msg = std::string(msg) + " - Message #" + std::to_string(i + 1);
+            EXPECT_STREQ(res, expected_msg.c_str());
         }
-        const char* res = static_cast<const char*>(rec.get());
-        std::string expected_msg = std::string(base_msg) + " - Message #" + std::to_string(i + 1);
-        EXPECT_STREQ(res, expected_msg.c_str());
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    ipc::node client_node("loop", ipc::LinkType::Sender, ipc::ChannelType::NamedPipe);
+    for (int i = 0; i < 20; ++i) {
+        std::string full_msg = std::string(msg) + " - Message #" + std::to_string(i + 1);
+        EXPECT_TRUE(client_node.send(full_msg.c_str(), full_msg.size() + 1)); // +1 for null terminator
     }
+
+    server_thread.join();
 }
 
 void test_struct()
@@ -67,20 +81,28 @@ void test_struct()
     msg.num = 1;
     strcpy_s(msg.mtext, "Hello, IPC with struct!");
 
-    ipc::node node("struct", ipc::ChannelType::NamedPipe);
-    EXPECT_TRUE(node.send(&msg, sizeof(msg)));
+    std::thread server_thread([msg]() {
+        ipc::node server_node("struct", ipc::LinkType::Receiver, ipc::ChannelType::NamedPipe);
+        auto rec = server_node.receive();
+        if (!rec) {
+            fprintf(stderr, "Server failed to receive message\n");
+            exit(1);
+        }
+        auto received_msg = static_cast<message*>(rec.get());
+        EXPECT_EQ(received_msg->meta_info.id, msg.meta_info.id);
+        EXPECT_STREQ(received_msg->meta_info.name, msg.meta_info.name);
+        EXPECT_DOUBLE_EQ(received_msg->meta_info.value, msg.meta_info.value);
+        EXPECT_EQ(received_msg->num, msg.num);
+        EXPECT_STREQ(received_msg->mtext, msg.mtext);
+    });
 
-    auto rec = node.receive();
-    if (!rec) {
-        fprintf(stderr, "Failed to receive message\n");
-        exit(1);
-    }
-    auto received_msg = static_cast<message*>(rec.get());
-    EXPECT_EQ(received_msg->meta_info.id, msg.meta_info.id);
-    EXPECT_STREQ(received_msg->meta_info.name, msg.meta_info.name);
-    EXPECT_DOUBLE_EQ(received_msg->meta_info.value, msg.meta_info.value);
-    EXPECT_EQ(received_msg->num, msg.num);
-    EXPECT_STREQ(received_msg->mtext, msg.mtext);
+    // Ensure that the server is started and waiting for connection
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    ipc::node client_node("struct", ipc::LinkType::Sender, ipc::ChannelType::NamedPipe);
+    EXPECT_TRUE(client_node.send(&msg, sizeof(msg)));
+
+    server_thread.join();
 }
 
 void test_class()
@@ -101,18 +123,26 @@ void test_class()
 
     MyClass obj(1, "Test Class", 3.14);
 
-    ipc::node node("class", ipc::ChannelType::NamedPipe);
-    EXPECT_TRUE(node.send(&obj, sizeof(MyClass)));
+    std::thread server_thread([obj]() {
+        ipc::node server_node("class", ipc::LinkType::Receiver, ipc::ChannelType::NamedPipe);
+        auto rec = server_node.receive();
+        if (!rec) {
+            fprintf(stderr, "Server failed to receive message\n");
+            exit(1);
+        }
+        auto received_obj = static_cast<MyClass*>(rec.get());
+        EXPECT_EQ(received_obj->id, obj.id);
+        EXPECT_STREQ(received_obj->name, obj.name);
+        EXPECT_DOUBLE_EQ(received_obj->value, obj.value);
+    });
 
-    auto rec = node.receive();
-    if (!rec) {
-        fprintf(stderr, "Failed to receive message\n");
-        exit(1);
-    }
-    auto received_obj = static_cast<MyClass*>(rec.get());
-    EXPECT_EQ(received_obj->id, obj.id);
-    EXPECT_STREQ(received_obj->name, obj.name);
-    EXPECT_DOUBLE_EQ(received_obj->value, obj.value);
+    // Ensure that the server is started and waiting for connection
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    ipc::node client_node("class", ipc::LinkType::Sender, ipc::ChannelType::NamedPipe);
+    EXPECT_TRUE(client_node.send(&obj, sizeof(MyClass)));
+
+    server_thread.join();
 }
 
 void test_subclass()
@@ -142,18 +172,26 @@ void test_subclass()
 
     Derived obj(1, "Test Subclass", 2.718);
 
-    ipc::node node("subclass", ipc::ChannelType::NamedPipe);
-    EXPECT_TRUE(node.send(&obj, sizeof(Derived)));
+    std::thread server_thread([obj]() {
+        ipc::node server_node("subclass", ipc::LinkType::Receiver, ipc::ChannelType::NamedPipe);
+        auto rec = server_node.receive();
+        if (!rec) {
+            fprintf(stderr, "Server failed to receive message\n");
+            exit(1);
+        }
+        auto received_obj = static_cast<Derived*>(rec.get());
+        EXPECT_EQ(received_obj->id, obj.id);
+        EXPECT_STREQ(received_obj->name, obj.name);
+        EXPECT_DOUBLE_EQ(received_obj->value, obj.value);
+    });
 
-    auto rec = node.receive();
-    if (!rec) {
-        fprintf(stderr, "Failed to receive message\n");
-        exit(1);
-    }
-    auto received_obj = static_cast<Derived*>(rec.get());
-    EXPECT_EQ(received_obj->id, obj.id);
-    EXPECT_STREQ(received_obj->name, obj.name);
-    EXPECT_DOUBLE_EQ(received_obj->value, obj.value);
+    // Ensure that the server is started and waiting for connection
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    ipc::node client_node("subclass", ipc::LinkType::Sender, ipc::ChannelType::NamedPipe);
+    EXPECT_TRUE(client_node.send(&obj, sizeof(Derived)));
+
+    server_thread.join();
 }
 
 TEST(MSGQ, basic)
