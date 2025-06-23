e12f3e1651e99241fc3f144f1f92df343fc75ec3
Jialong Liu <liujl01@sjtu.edu.cn>
2025-06-23 16:55:27 +0800

    [improve]: pipe reconnect support

--- include/ipc/pipe/pipe.h
+++ include/ipc/pipe/pipe.h
@@ -16,11 +16,13 @@ public:
     bool send(const void* data, size_t data_size);
     std::shared_ptr<void> receive();
     bool remove();
+    void connect();
 
 private:
     HANDLE pipe_;
     std::string pipe_name_;
     LinkType link_type_;
+    bool connected_;
     static const DWORD BUFFER_SIZE = 4096; // Default buffer size for named pipe communication
 };
 } // namespace pipe
--- src/pipe/pipe.cpp
+++ src/pipe/pipe.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
+#include <thread>
 
 #include "ipc/assert.h"
 #include "ipc/pipe/pipe.h"
@@ -11,25 +12,17 @@ namespace pipe {
 
 named_pipe::named_pipe(const std::string& name, LinkType ltype)
     : pipe_(INVALID_HANDLE_VALUE)
-    , link_type_(ltype)
     , pipe_name_(R"(\\.\pipe\)" + name)
+    , link_type_(ltype)
+    , connected_(false)
 {
     switch (ltype) {
     case LinkType::Unknown:
         ASSERT_RETURN(true, , "Link type must be specified in named_pipe constructor");
         break;
     case LinkType::Sender:
-        ASSERT_RETURN(!WaitNamedPipeA(pipe_name_.c_str(), NMPWAIT_USE_DEFAULT_WAIT), , "No Read Pipe Accessible");
-        pipe_ = CreateFileA(
-            pipe_name_.c_str(),
-            GENERIC_READ | GENERIC_WRITE,
-            FILE_SHARE_READ | FILE_SHARE_WRITE,
-            NULL,
-            OPEN_EXISTING,
-            0,
-            NULL);
-
-        ASSERT_RETURN(pipe_ == INVALID_HANDLE_VALUE, , "CreateFileA failed, erro = %lu\n", GetLastError());
+        // Move connection establishment to send method
+        // Prevent errors caused by not creating a receiver during initialization
         break;
     case LinkType::Receiver:
         pipe_ = CreateNamedPipeA(
@@ -42,12 +35,9 @@ named_pipe::named_pipe(const std::string& name, LinkType ltype)
             0, // Default timeout
             NULL);
 
-        ASSERT_RETURN(pipe_ == INVALID_HANDLE_VALUE, , "CreateNamedPipeA failed, error = %lu\n", GetLastError());
-        if (!ConnectNamedPipe(pipe_, NULL)) {
-            fprintf(stderr, "ConnectNamedPipe failed, error: %lu\n", GetLastError());
-            CloseHandle(pipe_);
-            pipe_ = INVALID_HANDLE_VALUE;
-        }
+        ASSERT_RETURN(pipe_ == INVALID_HANDLE_VALUE, , "CreateNamedPipeA failed, error = %lu", GetLastError());
+        // Move connection establishment to receive method
+        // Prevent blocking during initialization
         break;
     default:
         ASSERT_RETURN(true, , "Unknown link type in named_pipe constructor");
@@ -62,34 +52,68 @@ named_pipe::~named_pipe()
 
 bool named_pipe::send(const void* data, size_t data_size)
 {
+    if (!connected_)
+        connect();
     ASSERT_RETURN(!data, false, "Data is null");
 
     DWORD bytesWritten;
-    BOOL success = WriteFile(
-        pipe_,
-        data,
-        static_cast<DWORD>(data_size),
-        &bytesWritten,
-        NULL);
+    while (true) {
+        BOOL success = WriteFile(
+            pipe_,
+            data,
+            static_cast<DWORD>(data_size),
+            &bytesWritten,
+            NULL);
 
-    ASSERT_RETURN(!success, false, "WriteFile failed, error = %lu", GetLastError());
+        if (!success && GetLastError() == ERROR_PIPE_NOT_CONNECTED) {
+            // The pipe has been closed by the other end
+            printf("[ipc/pipe: sender] The pipe has been ended.\n");
+            printf("[ipc/pipe: sender] Waiting for receiver connection...\n");
+            connected_ = false;
+            // Reset the current pipeline instance and reconnect
+            DisconnectNamedPipe(pipe_);
+            connect();
+            // Try sending data again after reconnecting
+            continue;
+        }
+        ASSERT_RETURN(!success, false, "WriteFile failed, error = %lu", GetLastError());
+        break;
+    }
+
     ASSERT_RETURN(bytesWritten != data_size, false, "WriteFile did not write all data, expected: %zu, written: %lu", data_size, bytesWritten);
-
     return true;
 }
 
 std::shared_ptr<void> named_pipe::receive()
 {
+    if (!connected_)
+        connect();
+
     char buffer[BUFFER_SIZE];
     DWORD bytesRead;
 
-    BOOL success = ReadFile(
-        pipe_,
-        buffer,
-        BUFFER_SIZE,
-        &bytesRead,
-        NULL);
-    ASSERT_RETURN(!success, nullptr, "ReadFile failed, error = %lu", GetLastError());
+    while (true) {
+        BOOL success = ReadFile(
+            pipe_,
+            buffer,
+            BUFFER_SIZE,
+            &bytesRead,
+            NULL);
+        if (!success && GetLastError() == ERROR_BROKEN_PIPE) {
+            // The pipe has been closed by the other end
+            printf("[ipc/pipe: receiver] The pipe has been ended.\n");
+            printf("[ipc/pipe: receiver] Waiting for new connection...\n");
+            connected_ = false;
+            // Reset the current pipeline instance and reconnect
+            DisconnectNamedPipe(pipe_);
+            connect();
+            // Try receiving data again after reconnecting
+            continue;
+        }
+
+        ASSERT_RETURN(!success, nullptr, "ReadFile failed, error = %lu", GetLastError());
+        break;
+    }
 
     std::shared_ptr<void> result(malloc(bytesRead), free);
     ASSERT_RETURN(!result, nullptr, "malloc failed");
@@ -102,11 +126,46 @@ bool named_pipe::remove()
 {
     if (pipe_ != INVALID_HANDLE_VALUE) {
         DisconnectNamedPipe(pipe_);
-        CloseHandle(pipe_);
+        if (link_type_ == LinkType::Receiver)
+            CloseHandle(pipe_);
         pipe_ = INVALID_HANDLE_VALUE;
+        connected_ = false;
     }
 
     return true;
 }
 
-} // namespace msgq
+void named_pipe::connect()
+{
+    if (link_type_ == LinkType::Sender) {
+        ASSERT_RETURN(!WaitNamedPipeA(pipe_name_.c_str(), NMPWAIT_USE_DEFAULT_WAIT), , "No Named Pipe Accessible");
+        pipe_ = CreateFileA(
+            pipe_name_.c_str(),
+            GENERIC_READ | GENERIC_WRITE,
+            FILE_SHARE_READ | FILE_SHARE_WRITE,
+            NULL,
+            OPEN_EXISTING,
+            0,
+            NULL);
+
+        ASSERT_RETURN(pipe_ == INVALID_HANDLE_VALUE, , "CreateFileA failed, erro = %lu", GetLastError());
+        connected_ = true;
+    }
+
+    if (link_type_ == LinkType::Receiver) {
+        if (!ConnectNamedPipe(pipe_, NULL)) {
+            DWORD err = GetLastError();
+            if (err == ERROR_PIPE_CONNECTED) {
+                connected_ = true;
+            } else {
+                printf("ConnectNamedPipe failed, error = %lu\n", err);
+                // Reset the pipeline for the next connection
+                DisconnectNamedPipe(pipe_);
+            }
+        } else {
+            connected_ = true;
+        }
+    }
+}
+
+} // namespace pipe
